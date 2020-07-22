869d8542cf35b480ddbbb1e9757ef5fad58510da
Bruno Borghi <bruno.borghi@gmail.com>
2020-07-21 21:54:55 -0300

    Commit da primeira aula do curso 2 de Python 3

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,14 @@ def jogar():
     print("***Bem vindo no jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando ...")
+
     print("Fim do jogo.")
 
 if (__name__ == "__main__"):
